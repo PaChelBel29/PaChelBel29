c83e123201f1dfdec31201e59129d085f4d67b61
PaChelBel29 <51313633+PaChelBel29@users.noreply.github.com>
2022-05-18 13:42:21 +0900

    Add files via upload

---
+++ BitCoinSimulator/BitCoinSimulator/Commands/SaveCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace BitCoinSimulator.Commands
+{
+    class SaveCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //구현해야 함
+        }
+    }
+}
---
+++ BitCoinSimulator/BitCoinSimulator/Commands/StartCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using BitCoinSimulator.Frames;
+
+namespace BitCoinSimulator.Commands
+{
+    class StartCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (!Properties.Data_Save_File.Default.is_Saved)
+            {
+                return true;
+               
+                
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            
+            //LoadCommand load = new LoadCommand();
+            ////load.Execute();
+            //return false;
+        }
+    }
+}
